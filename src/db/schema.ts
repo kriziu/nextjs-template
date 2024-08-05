536bccb34215768386ee05bac6ed69965af9416e
Bruno Dziecielski <dziecielskibruno@gmail.com>
2024-08-05 12:42:19 +0200

    test: check coolify autodeploy

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,7 @@
-import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
+import { integer, pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   name: varchar('name', { length: 256 }),
+  age: integer('age').default(20),
 });
